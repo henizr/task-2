26d7b0241894555b3344714c1deca864eea750a3
henizr <henry.iz@yandex.ru>
2020-04-05 18:59:58 +0200

    Добавлен блок color-sections и sass переменные

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const importAll =  (r) => r.keys().forEach(r);
 
-importAll(require.context('./', true, /\.scss$/));
+importAll(require.context('./sass', true, /\.scss$/));
+importAll(require.context('./blocks', true, /\.scss$/));
+importAll(require.context('./pages', true, /\.scss$/));
